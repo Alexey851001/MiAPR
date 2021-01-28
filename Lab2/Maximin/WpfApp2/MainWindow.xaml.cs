79afd36656e4dd278c5e275e9b1024564bd12795
ListratenkoIgor <alastor_rujichka@mail.ru>
2021-01-28 18:33:25 +0300

    Lab-2:Add drawing by iteration
    Previous iteration NumPad4. Next iteration NumPad6.  Start with last iteration

---
+++ Lab2/Maximin/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Maximin;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Brush[] _brushes = new[] { Brushes.Blue, Brushes.Brown, Brushes.Chartreuse, Brushes.Chocolate, Brushes.Cyan, Brushes.Gold, Brushes.Gray, Brushes.Fuchsia, Brushes.Green, Brushes.Indigo, Brushes.Lime, Brushes.Maroon, Brushes.Orchid, Brushes.Red, Brushes.Salmon, Brushes.Silver, Brushes.Teal, Brushes.Tomato, Brushes.DarkGreen, Brushes.Bisque };
+        private static int Iterator;
+        private List<List<Area>> Areas = new List<List<Area>>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Areas.Add(new List<Area>(ClassesGenerator.Initialize(1000, 8)));           
+            while (!ClassesGenerator.EndIteration()) {
+                Areas.Add(new List<Area>(ClassesGenerator.Iteration()));
+            }
+
+            Iterator = Areas.Count - 1;
+            DrawAreas();
+        }
+        private void Iteration_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.NumPad6) { Right_Click(null, null); }
+            if (e.Key == Key.NumPad4) { Left_Click(null, null); }
+        }
+        private void Right_Click(object sender, RoutedEventArgs e)
+        {
+            if (Iterator+1 < Areas.Count) {
+                Iterator++;
+                DrawAreas();
+            } 
+        }
+        private void Left_Click(object sender, RoutedEventArgs e)
+        {
+            if (Iterator>0)
+            {
+                Iterator--;
+                DrawAreas();
+            }
+        }
+        private void DrawAreas() {
+            int index = 0;
+            List<Area> areas = Areas[Iterator];
+            grid1.Children.Clear();
+            foreach (var area in areas)
+            {
+                Brush brush = _brushes[index];
+                foreach (var point in area.AreaPoints)
+                {
+                    DrawPoint(point, brush);
+                }
+
+                DrawPoint(area.Kernel, Brushes.Black);
+                index++;
+            }
+
+        }
+        public void DrawPoint(Maximin.Point point, Brush brush)
+        {            
+            Ellipse elipse = new Ellipse();
+
+            elipse.Width = 8;
+            elipse.Height = 8;
+
+            elipse.StrokeThickness = 4;
+            elipse.Stroke = brush;
+            elipse.Margin = new Thickness(point.X - 500, point.Y - 500, 0, 0);
+
+            grid1.Children.Add(elipse);
+        }
+    }
+}
