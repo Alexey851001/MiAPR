68913c29cb7df942224850eb924c432f30fd4570
ListratenkoIgor <alastor_rujichka@mail.ru>
2021-01-28 23:23:58 +0300

    Update Program.cs

--- Lab2/Maximin/TestApp/Program.cs
+++ Lab2/Maximin/TestApp/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using KMeans;
 using Maximin;
+using Generator;
 namespace TestApp
 {
     class Program
     {
-        public static void WriteAreas(List<Area> areas)
+        public static void WriteAreas(List<IArea> areas)
         {
             int j = 0;
             foreach (var area in areas)
@@ -14,6 +16,16 @@ namespace TestApp
                 Console.WriteLine($"Area {j}: {area.AreaPoints.Count} points. Kernel {area.Kernel.X},{area.Kernel.Y}");
             }
         }
+        public static List<IArea> ConvertToIArea(List<Maximin.Area> areas) {
+            List<IArea> result = new List<IArea>();
+            areas.ForEach(area => result.Add(area));
+            return result;
+        } 
+        public static List<IArea> ConvertToIArea(List<KMeans.Area> areas) {
+            List<IArea> result = new List<IArea>();
+            areas.ForEach(area => result.Add(area));
+            return result;
+        } 
         static void Main(string[] args)
         {
             //Console.WriteLine("Enter count of points");
@@ -21,33 +33,45 @@ namespace TestApp
             //Console.WriteLine("Enter count of class");
             //int classCount = Console.Read();
 
-            int pointCount = 200000;
+            int pointCount = 20000;
             int classCount = 8;
-            int circles = 200;
-            List<Area> areas;
+            List<IArea> areas;               
             //requared initialization
-            while (circles > 0)
-            {                
-                Console.WriteLine("Initialize");
-                areas = ClassesGenerator.Initialize(pointCount, classCount);
+            Console.WriteLine("Initialize");
+            areas = ConvertToIArea(Maximin.ClassesGenerator.Initialize(pointCount));
+            WriteAreas(areas);
+            //step-by-step generation
+            int iterations = 0;
+            Console.WriteLine("start step-by-step generation"); 
+            while (!Maximin.ClassesGenerator.EndIteration())
+            {
+                areas = ConvertToIArea(Maximin.ClassesGenerator.Iteration2());
+               
+                iterations++;
+                Console.WriteLine($"iteration {iterations}.");
                 WriteAreas(areas);
-                //step-by-step generation
-                int iterations = 0;
-                Console.WriteLine("start step-by-step generation"); 
-                while (!ClassesGenerator.EndIteration())
-                {
-                    areas = ClassesGenerator.Iteration();
-                    iterations++;
-                    Console.WriteLine($"iteration {iterations}.");
-                    WriteAreas(areas);
-                }
-                Console.WriteLine($"iterate {iterations} times\n\n\n");
-                circles--;
             }
+            Console.WriteLine($"iterate {iterations} times\n\n\n");
+            //List<KMeans.Area> Kareas;
+            //requared initialization
+            Console.WriteLine("Initialize");
+            areas = ConvertToIArea(KMeans.ClassesGenerator.Initialize(areas,Maximin.ClassesGenerator.GetPoints()));
+            WriteAreas(areas);
+            //step-by-step generation
+            Console.WriteLine("start step-by-step generation");
+            while (!KMeans.ClassesGenerator.EndIteration())
+            {
+                areas = ConvertToIArea(KMeans.ClassesGenerator.Iteration());
+                iterations++;
+                Console.WriteLine($"iteration {iterations}.");
+                WriteAreas(areas);
+            }
+            Console.WriteLine($"iterate {iterations} times\n\n\n");
             //full generation
             //Console.WriteLine("start full generation");
             //areas = ClassesGenerator.Generate();
             //WriteAreas(areas);            
         }
+
     }
 }
