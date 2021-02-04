67717eb4a4f389a0a3b1b800c882967dfffd8cdf
Alexey Timofievich <alex.timofievich@gmail.com>
2021-02-04 14:34:09 +0300

    Add wpf and generation classes

---
+++ Lab4/ConsoleApp1/PerseptronLib/Perseptron.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PerseptronLib
+{
+    public class Perseptron
+    {
+        private int _classCount;
+        private int _instanseCount;
+        private int _signCount;
+        private List<PerseptronClass> _classes = new List<PerseptronClass>();
+        
+        public void Evaluate(out string classes, out string functions)
+        {
+            classes = this.ToString();
+            
+            functions = "2";
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = 0; i < _classCount; i++)
+            {
+                result += "Класс " + (i+1) + " \n\r" + _classes[i].ToString();
+
+            }
+            return result;
+        }
+
+        public Perseptron(int classCount, int instanseCount, int signCount)
+        {
+            _classCount = classCount;
+            _instanseCount = instanseCount;
+            _signCount = signCount;
+
+            for (int i = 0; i < classCount; i++)
+            {
+                PerseptronClass perseptronClass = new PerseptronClass(instanseCount, signCount);
+                _classes.Add(perseptronClass);
+            }
+
+        }
+    }
+
+    public class PerseptronClass
+    {
+        public List<PerseptronInstanse> Instanses = new List<PerseptronInstanse>();
+
+        public PerseptronClass(int countOfInstanse, int countOfSign)
+        {
+            for (int i = 0; i < countOfInstanse; i++)
+            {
+                PerseptronInstanse instanse = new PerseptronInstanse(countOfSign);
+                Instanses.Add(instanse);
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+
+            for (int i = 0; i < Instanses.Count; i++)
+            {
+                result += "\tОбраз " + (i+1) + Instanses[i].ToString();
+            }
+            return result;
+        }
+    }
+
+    public class PerseptronInstanse
+    {
+        public List<int> Signs = new List<int>();
+
+        private Random _random = new Random();
+        private int _range = 10;
+
+        public PerseptronInstanse(int countOfSign)
+        {
+            for (int i = 0; i < countOfSign; i++)
+            {
+                int sign = _random.Next() % _range - _range / 2;
+                Signs.Add(sign);
+            }
+        }
+
+        public override string ToString()
+        {
+            string result = " (";
+
+            for (int i = 0; i < Signs.Count; i++)
+            {
+                if (i != Signs.Count - 1)
+                {
+                    result += Signs[i] + ",";
+                }
+                else
+                {
+                    result += Signs[i];
+                }
+            }
+
+            result += ")\n\r";
+            return result;
+        }
+    }
+    
+}
---
+++ Lab4/ConsoleApp1/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PerseptronLib;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextClassCount.Text = "3";
+            TextInstanceCount.Text = "1";
+            TextSignCount.Text = "2";
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+            Perseptron perseptron = new Perseptron(Convert.ToInt32(TextClassCount.Text),
+                Convert.ToInt32(TextInstanceCount.Text),
+                Convert.ToInt32(TextSignCount.Text));
+            perseptron.Evaluate(out string classes, out string functions);
+            ClassesBox.Text = classes;
+            FunctionBox.Text = functions;
+        }
+    }
+}
